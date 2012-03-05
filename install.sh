d52de6f8fdb65880cda40eedca229b6176e93fec
munirmussa <munirmussa@gmail.com>
2012-03-05 20:28:41 -0300

    Adicionado script de instalação

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git
+sudo apt-get install python2.7
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+pip install django
+sudo easy_install -U django_evolution
+cd build/django
+sudo python setup.py install
+
