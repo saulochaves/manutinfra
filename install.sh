ab667ce9f75452b180a0ca596b9a2abd32849d2c
Munir Mussa <munirmussa@gmail.com>
2012-03-08 22:29:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
+#Instalando o git
 sudo apt-get install git
+
+#Instalando a versão 2.7 do python
 sudo apt-get install python2.7
-sudo apt-get install python-setuptools
+
+#Instalando o pip e algumas ferramentas do python
 sudo apt-get install python-pip
+sudo apt-get install python-setuptools
+
+#Baixando o Django
 pip install django
+
+#Instalando o Django Evolution
 sudo easy_install -U django_evolution
+
+#Instalando o Django
 cd build/django
 sudo python setup.py install
 
